975587547bd9634d0238bbb5ff76e1ebf7cff096
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-31 14:01:47 +0200

    START- EDIT struttura

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<?php
+
+//1.classe Prodotto
+
+
+//2. classe Categoria
+
+
+//3. classe Shop
+
+
+//4. Categorie Cani o Gatti
+
+
+//5. tipi di prodotti "Giochino per gatti" - "Cuccetta per cani" - "Croccantini per gatti sterilizzati''
+
+
+//6. shop
+
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <title>Zoo E-commerce</title>
+</head>
+<body>
+
+  <div class="container">
+
+  <!-- 7.CARD IN FOREACH -->
+
+  </div>
+    
+</body>
+</html>
